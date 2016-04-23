978bd09890714faa33249bdc8a5c7aee008ae24e
kickinbahk <kickinbahk@gmail.com>
2016-04-23 12:51:35 -0700

    refactor client request to run fully async

--- client.js
+++ client.js
@@ -1,36 +1,29 @@
 'use strict'
 
 var request = require('request')
+var async = require('async')
 
-var name
-var status
-var firstHasFinished = false
-var secondHasFinished = false
-
-request.get(
-  'http://localhost:8080/getUserName?id=1234',
-  function (err, res, body) {
-    var result = JSON.parse(body)
-    name = result.value
-    markFinished('first')
-  }
-)
-request.get(
-  'http://localhost:8080/getUserStatus?id=1234',
-  function (err, res, body) {
-    var result = JSON.parse(body)
-    status = result.value
-    markFinished('second')
-  }
-)
-function markFinished (step) {
-  if (step === 'first') {
-    firstHasFinished = true
-  }
-  if (step === 'second') {
-    secondHasFinished = true
-  }
-  if (firstHasFinished && secondHasFinished) {
-    console.log(`The status of user ${name} is ${status}`)
-  }
+var getUsername = function (callback) {
+  request.get(
+    'http://localhost:8080/getUserName?id=1234',
+    function (err, res, body) {
+      var result = JSON.parse(body)
+      callback(err, result.value)
+    })
 }
+
+var getUserStatus = function (callback) {
+  request.get(
+    'http://localhost:8080/getUserStatus?id=1234',
+    function (err, res, body) {
+      var result = JSON.parse(body)
+      callback(err, result.value)
+    })
+}
+
+async.parallel(
+  [getUsername, getUserStatus],
+  function (err, results) {
+    console.log(`The status of user ${results[0]} is ${results[1]}`)
+  }
+)
