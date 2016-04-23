f3e19891a374a3885056ba2a65f47dc2fb340c05
kickinbahk <kickinbahk@gmail.com>
2016-04-23 12:36:37 -0700

    change implementation to be more async

--- client.js
+++ client.js
@@ -2,19 +2,35 @@
 
 var request = require('request')
 
+var name
+var status
+var firstHasFinished = false
+var secondHasFinished = false
+
 request.get(
   'http://localhost:8080/getUserName?id=1234',
   function (err, res, body) {
     var result = JSON.parse(body)
-    var name = result.value
-
-    request.get(
-      'http://localhost:8080/getUserStatus?id=1234',
-      function (err, res, body) {
-        var result = JSON.parse(body)
-        var status = result.value
-
-        console.log(`The status of user ${name} is ${status}`)
-      })
+    name = result.value
+    markFinished('first')
   }
 )
+request.get(
+  'http://localhost:8080/getUserStatus?id=1234',
+  function (err, res, body) {
+    var result = JSON.parse(body)
+    status = result.value
+    markFinished('second')
+  }
+)
+function markFinished (step) {
+  if (step === 'first') {
+    firstHasFinished = true
+  }
+  if (step === 'second') {
+    secondHasFinished = true
+  }
+  if (firstHasFinished && secondHasFinished) {
+    console.log(`The status of user ${name} is ${status}`)
+  }
+}
