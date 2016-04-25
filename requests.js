4afab92ef64379315b2e5c205583e16518081d00
kickinbahk <kickinbahk@gmail.com>
2016-04-25 07:37:10 -0700

    update request.js async code

--- requests.js
+++ requests.js
@@ -1,21 +1,46 @@
 'use strict'
 
 var request = require('request')
+var async = require('async')
 
 var url = 'http://localhost:8080'
 
-request.get(`${url}getUserName?id=1234`, function (err, res, body) {
-  console.log('Status:', JSON.parse(body).value)
-
-  request.get(`${url}getUserStatus?id=1234`, function (err, res, body) {
-    console.log('Status:', JSON.parse(body).value)
-
-    request.get(`${url}getUserCountry?id=1234`, function (err, res, body) {
-      console.log('Status:', JSON.parse(body).value)
-
-      request.get(`${url}getUserAge?id=1234`, function (err, res, body) {
-        console.log('Status:', JSON.parse(body).value)
-      })
+async.series([
+  function (callback) {
+    request.get(`${url}getUserName?id=1234`, function (err, res, body) {
+      console.log('Name:', JSON.parse(body).value)
+      callback(null)
     })
-  })
-})
+  }, function (callback) {
+    request.get(`${url}getUserStatus?id=1234`, function (err, res, body) {
+      console.log('Status:', JSON.parse(body).value)
+      callback(null)
+    })
+  }, function (callback) {
+    request.get(`${url}getUserCountry?id=1234`, function (err, res, body) {
+      console.log('Country:', JSON.parse(body).value)
+      callback(null)
+    })
+  }, function (callback) {
+    request.get(`${url}getUserAge?id=1234`, function (err, res, body) {
+      console.log('Age:', JSON.parse(body).value)
+      callback(null)
+    })
+  }
+])
+
+// request.get(`${url}getUserName?id=1234`, function (err, res, body) {
+//   console.log('Name:', JSON.parse(body).value)
+//
+//   request.get(`${url}getUserStatus?id=1234`, function (err, res, body) {
+//     console.log('Status:', JSON.parse(body).value)
+//
+//     request.get(`${url}getUserCountry?id=1234`, function (err, res, body) {
+//       console.log('Country:', JSON.parse(body).value)
+//
+//       request.get(`${url}getUserAge?id=1234`, function (err, res, body) {
+//         console.log('Age:', JSON.parse(body).value)
+//       })
+//     })
+//   })
+// })
