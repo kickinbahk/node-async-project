9bcaac79d097986edd251655238207ef8bb5abd9
kickinbahk <kickinbahk@gmail.com>
2016-04-25 07:50:19 -0700

    add other async method

--- requests.js
+++ requests.js
@@ -5,30 +5,50 @@ var async = require('async')
 
 var url = 'http://localhost:8080'
 
-async.series([
+// ** Async Waterfall **
+
+async.waterfall([
   function (callback) {
-    request.get(`${url}getUserName?id=1234`, function (err, res, body) {
-      console.log('Name:', JSON.parse(body).value)
-      callback(null)
+    request.get(`${url}getSessionId`, function (err, res, body) {
+      callback(null, JSON.parse(body).value)
     })
-  }, function (callback) {
-    request.get(`${url}getUserStatus?id=1234`, function (err, res, body) {
-      console.log('Status:', JSON.parse(body).value)
-      callback(null)
+  }, function (sId, callback) {
+    request.get(`${url}getUserId?sessionId=${sId}`, function (err, res, body) {
+      callback(null, JSON.parse(body).value)
     })
-  }, function (callback) {
-    request.get(`${url}getUserCountry?id=1234`, function (err, res, body) {
-      console.log('Country:', JSON.parse(body).value)
-      callback(null)
+  }, function (sId, uId, callback) {
+    request.get(`${url}getUserName?userId=${uId}`, function (err, res, body) {
+      callback(null, JSON.parse(body).value, sId)
     })
-  }, function (callback) {
-    request.get(`${url}getUserAge?id=1234`, function (err, res, body) {
-      console.log('Age:', JSON.parse(body).value)
-      callback(null)
-    })
+  }, function (err, res, sId) {
+    console.log('Name:', name)
+    console.log('sessionID', sId)
   }
 ])
 
+// ** Async Series **
+// async.series([
+//   function (callback) {
+//     request.get(`${url}getUserName?id=1234`, function (err, res, body) {
+//       console.log()
+//       callback(null, 'Name:' + JSON.parse(body).value)
+//     })
+//   }, function (callback) {
+//     request.get(`${url}getUserStatus?id=1234`, function (err, res, body) {
+//       callback(null, 'Status:' + JSON.parse(body).value)
+//     })
+//   }, function (callback) {
+//     request.get(`${url}getUserCountry?id=1234`, function (err, res, body) {
+//       callback(null, 'Country:' + JSON.parse(body).value)
+//     })
+//   }, function (callback) {
+//     request.get(`${url}getUserAge?id=1234`, function (err, res, body) {
+//       callback(null, 'Age:' + JSON.parse(body).value)
+//     })
+//   }
+// ])
+
+// ** Callbacks **
 // request.get(`${url}getUserName?id=1234`, function (err, res, body) {
 //   console.log('Name:', JSON.parse(body).value)
 //
